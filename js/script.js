1528ebd0b1436807fb5c760cf5c2bb97aa1edd08
fazliddinrustamov <azizyuldashev19800@gmail.com>
2022-08-06 20:42:33 +0500

    add site codes

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var elForm = document.querySelector(".way-form");
+var elInput = document.querySelector(".way-input");
+var elBtn = document.querySelector(".way-btn");
+var elScoreList = document.querySelector(".score-result");
+var elLiFoot = elScoreList.querySelector(".result-item-foot");
+var elLiBike = elScoreList.querySelector(".result-item-bike");
+var elLiCar = elScoreList.querySelector(".result-item-car");
+var elLiTrain = elScoreList.querySelector(".result-item-train");
+
+var footSpeed = 10;
+var bikeSpeed = 20;
+var carSpeed = 60;
+var trainSpeed = 100;
+
+elForm.addEventListener("submit", function(evt){
+  evt.preventDefault()
+
+  elInputValue = Number(elInput.value);
+  elInput.value = "";
+
+  var footTime = (elInputValue / footSpeed).toFixed(1);
+  var bikeTime = (elInputValue / bikeSpeed).toFixed(1);
+  var carTime = (elInputValue / carSpeed).toFixed(1);
+  var trainTime = (elInputValue / trainSpeed).toFixed(1);
+
+  document.querySelector(".result-item-foot").textContent = `${footTime} hours`;
+  document.querySelector(".result-item-bike").textContent = `${bikeTime} hours`;
+  document.querySelector(".result-item-car").textContent = `${carTime} hours`;
+  document.querySelector(".result-item-train").textContent = `${trainTime} hours`;
+})
